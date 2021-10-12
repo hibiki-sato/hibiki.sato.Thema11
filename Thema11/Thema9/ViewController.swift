d85bb1a6564807bda63cb61038602a740bf2e704
hibiki-sato <82596100+hibiki-sato@users.noreply.github.com>
2021-10-12 22:17:42 +0900

    Add files via upload

---
+++ Thema11/Thema9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Thema9
+//
+//  Created by 佐藤響 on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var todohukenLabel: UILabel!
+    @IBOutlet weak var inputLabel: UILabel!
+
+    @IBAction func inputButton(_ sender: Any) {
+    }
+
+    @IBAction func exit(segue: UIStoryboardSegue) {
+    }
+
+    @IBAction func didPressTodohuken(segue: UIStoryboardSegue) {
+        let listVC = segue.source as? ListViewController
+        if let indexPath = listVC?.tableView.indexPathForSelectedRow {
+            inputLabel.text = listVC?.prefectures[indexPath.row]
+        }
+    }
+}
